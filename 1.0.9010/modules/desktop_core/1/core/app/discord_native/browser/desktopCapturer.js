92dc065870fd50c8d656ff6fdef9c05befad7eaa
CanadaHonk <oj@oojmed.com>
2023-02-11 22:55:10 +0000

    reset for symlinks

--- 1.0.9010/modules/desktop_core/1/core/app/discord_native/browser/desktopCapturer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _electron = _interopRequireDefault(require("electron"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_electron.default.ipcMain.handle('DESKTOP_CAPTURER_GET_SOURCES', (_, opts) => _electron.default.desktopCapturer.getSources(opts));
--- 1.0.9010/modules/desktop_core/1/core/common/typings/global.d.ts
+++
@@ -1,6 +0,0 @@
-import NodeModule from 'module';
-
-declare module 'module' {
-  var globalPaths: string[];
-  var _resolveLookupPaths: (request: string, parent: NodeModule) => string[];
-}
